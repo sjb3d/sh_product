5406352b16eefb28392804294bc03c897aae89c1
Simon Brown <si@sjbrown.co.uk>
2022-02-13 09:13:00 +0000

    Simplify indexing

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,21 @@ fn legendre(l: i32, m: i32, cos_theta: f64, sin_theta: f64) -> f64 {
     legendre_impl(l, m.abs(), cos_theta, sin_theta)
 }
 
+fn lm_from_index(index: usize) -> (i32, i32)
+{
+    let mut l = 0;
+    loop {
+        let start = l*l;
+        let end = start + 2*l + 1;
+        if index < end {
+            let l = l as i32;
+            let m = ((index - start) as i32) - l;
+            return (l, m);
+        }
+        l += 1;
+    }
+}
+
 fn spherical_harmonic(l: i32, m: i32, phi: f64, cos_theta: f64, sin_theta: f64) -> f64 {
     let m_abs = m.abs();
     let const_sq = ((2 * l + 1) * (l - m_abs).factorial()) as f64
@@ -48,7 +63,7 @@ fn integrate<F>(f: F) -> f64
 where
     F: Fn(f64, f64, f64) -> f64,
 {
-    const PHI_STEPS: usize = 512;
+    const PHI_STEPS: usize = 256;
     const COS_THETA_STEPS: usize = PHI_STEPS / 2;
     let mut sum = 0.0;
     for cos_theta_index in 0..COS_THETA_STEPS {
@@ -62,59 +77,49 @@ where
     sum * (4.0 * PI) / ((PHI_STEPS * COS_THETA_STEPS) as f64)
 }
 
-fn first_m(prev_l: i32, prev_m: i32, l: i32) -> i32 {
-    if prev_l == l {
-        prev_m
-    } else {
-        -l
-    }
-}
+const MAX_ORDER: usize = 2;
+const COEFF_COUNT: usize = (MAX_ORDER + 1)*(MAX_ORDER + 1);
 
 const TOLERANCE: f64 = 0.001;
 
 fn main() {
-    for l1 in 0..=2 {
-        for m1 in (-l1)..=l1 {
-            for l2 in l1..=2 {
-                for m2 in first_m(l1, m1, l2)..=l2 {
-                    let integral = integrate(|phi, cos_theta, sin_theta| {
-                        spherical_harmonic(l1, m1, phi, cos_theta, sin_theta)
-                            * spherical_harmonic(l2, m2, phi, cos_theta, sin_theta)
-                    });
-                    let expected = if l1 == l2 && m1 == m2 { 1.0 } else { 0.0 };
-                    if (integral - expected).abs() > TOLERANCE {
-                        println!(
-                            "integral y_{}^{} * y_{}^{} failed: got {}, expected {}",
-                            l1, m1, l2, m2, integral, expected
-                        );
-                    }
-                }
+    for i1 in 0..COEFF_COUNT {
+        let (l1, m1) = lm_from_index(i1);
+        for i2 in i1..COEFF_COUNT {
+            let (l2, m2) = lm_from_index(i2);
+            let integral = integrate(|phi, cos_theta, sin_theta| {
+                spherical_harmonic(l1, m1, phi, cos_theta, sin_theta)
+                    * spherical_harmonic(l2, m2, phi, cos_theta, sin_theta)
+            });
+            let expected = if i1 == i2 { 1.0 } else { 0.0 };
+            if (integral - expected).abs() > TOLERANCE {
+                println!(
+                    "integral y_{}^{} * y_{}^{} failed: got {}, expected {}",
+                    l1, m1, l2, m2, integral, expected
+                );
             }
         }
     }
 
-    for l1 in 0..=2 {
-        for m1 in (-l1)..=l1 {
-            for l2 in l1..=2 {
-                for m2 in first_m(l1, m1, l2)..=l2 {
-                    for l3 in l2..=2 {
-                        for m3 in first_m(l2, m2, l3)..=l3 {
-                            let integral = integrate(|phi, cos_theta, sin_theta| {
-                                spherical_harmonic(l1, m1, phi, cos_theta, sin_theta)
-                                    * spherical_harmonic(l2, m2, phi, cos_theta, sin_theta)
-                                    * spherical_harmonic(l3, m3, phi, cos_theta, sin_theta)
-                            });
-                            if integral.abs() > TOLERANCE {
-                                println!(
-                                    "integral y_{}^{} * y_{}^{} * y_{}^{} = {}",
-                                    l1, m1, l2, m2, l3, m3, integral
-                                );
-                            }
-                        }
-                    }
+    for i1 in 0..COEFF_COUNT {
+        let (l1, m1) = lm_from_index(i1);
+        for i2 in i1..COEFF_COUNT {
+            let (l2, m2) = lm_from_index(i2);
+            for i3 in i2..COEFF_COUNT {
+                let (l3, m3) = lm_from_index(i3);
+                let integral = integrate(|phi, cos_theta, sin_theta| {
+                    spherical_harmonic(l1, m1, phi, cos_theta, sin_theta)
+                        * spherical_harmonic(l2, m2, phi, cos_theta, sin_theta)
+                        * spherical_harmonic(l3, m3, phi, cos_theta, sin_theta)
+                });
+                if integral.abs() > TOLERANCE {
+                    println!(
+                        "integral y_{}^{} * y_{}^{} * y_{}^{} = {}",
+                        l1, m1, l2, m2, l3, m3, integral
+                    );
                 }
             }
-            println!();
         }
+        println!();
     }
 }
